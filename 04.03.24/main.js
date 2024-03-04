2fe1d69f3c657eef7a449139b198924b88a19de1
quattrokki <160909419+quattrokki@users.noreply.github.com>
2024-03-04 17:59:37 +0600

    Add files via upload

---
+++ 04.03.24/main.js
@@ -0,0 +1,56 @@
+/*
+1) В массив const brandsArr = ['google', 'apple', 'microsoft'] добавьте строки 'xiaomi', 'huawei', 'oneplus'
+Для сохранения новых элементов используйте как минимум два разных способа, экспериментируйте и постарайтесь понять разницу между разными способами. 
+Выведите результаты всех найденных способов в консоль.
+*/
+
+// const brandsArr = ['google', 'apple', 'microsoft'];
+// brandsArr.push('xiaomi');
+
+
+// brandsArr.unshift('huawei');
+
+
+// brandsArr.splice(0,0,'oneplus');
+// console.log(brandsArr);
+
+
+/*
+2) Создайте массив имен и с добавьте в конец массива новое имя использую prompt, 
+после таким же образом добавляем второе имя, только уже в начало массива
+*/
+
+// let name1= prompt("Введите ваше имя");
+// let name2 = prompt("Введите ваше имя");
+
+// let mainList = ["Tariel", "Emil"];
+// mainList.unshift(name1);
+// mainList.push(name2);
+
+// console.log(mainList);
+
+
+/*
+3) У вас есть массив colors, содержащий цвета.
+ Используйте метод splice, чтобы удалить второй элемент массива и затем добавить новый элемент "green". Выведите обновленный массив в консоль.
+*/
+
+
+// let colors = ['red', 'blue', 'yellow']
+// console.log(colors);
+// colors.splice(2, 1, "green");
+// console.log(colors);
+
+
+/*
+4) У вас есть массив numbers, содержащий числа от 1 до 10. 
+Используйте метод slice, чтобы получить подмассив, содержащий элементы с пятого по восьмой включительно. Выведите полученный подмассив в консоль
+*/
+
+
+// let numbers = [1,2,3,4,5,6,7,8,9,10]
+// console.log("numbers", numbers);
+
+// let sliced = numbers.slice(4, 8);
+// console.log("sliced", sliced);
+
